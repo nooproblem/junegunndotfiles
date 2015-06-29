60c71a3e9165d058ee389fa862fd2a030dcc5de0
Junegunn Choi <junegunn.c@gmail.com>
2015-06-29 10:09:54 +0900

    eastwood

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 ;; https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [lein-licenses "0.1.1"]]
+                  [lein-licenses "0.1.1"]
+                  [jonase/eastwood "0.2.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
