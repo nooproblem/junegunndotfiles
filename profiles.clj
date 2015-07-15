3405fb705a24f62f3399aafbdc375a42905105b0
Junegunn Choi <junegunn.c@gmail.com>
2015-07-16 02:44:25 +0900

    profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,9 @@
 ;; https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-licenses "0.1.1"]
+                  [lein-marginalia "0.8.0"]                    ; lein marg
+                  [codox "0.8.13"]                             ; lein doc
+                  [com.jakemccrary/lein-test-refresh "0.10.0"] ; lein test-refresh
                   [jonase/eastwood "0.2.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
