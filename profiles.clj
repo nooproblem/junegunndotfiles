f7716d15125d47e1001f6b3c35716f1647b6c449
Junegunn Choi <junegunn.c@gmail.com>
2015-07-29 11:57:30 +0900

    pprint

--- profiles.clj
+++ profiles.clj
@@ -4,6 +4,7 @@
                   [lein-marginalia "0.8.0"]                    ; lein marg
                   [codox "0.8.13"]                             ; lein doc
                   [com.jakemccrary/lein-test-refresh "0.10.0"] ; lein test-refresh
+                  [lein-pprint "1.1.2"]
                   [jonase/eastwood "0.2.1"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
