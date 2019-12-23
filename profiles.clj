03222adc82cce49c6318675cceae636d185c733a
Junegunn Choi <junegunn.c@gmail.com>
2019-12-23 19:20:50 +0900

    lein ancient check-profiles

--- profiles.clj
+++ profiles.clj
@@ -1,21 +1,21 @@
 ;; https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md
-{:user {:plugins [[cider/cider-nrepl "0.16.0"]
-                  [lein-auto "0.1.3"] [lein-kibit "0.1.6"]     ; lein auto kibit
+{:user {:plugins [[cider/cider-nrepl "0.22.4"]
+                  [lein-auto "0.1.3"] [lein-kibit "0.1.8"]     ; lein auto kibit
                   [lein-marginalia "0.9.1"]                    ; lein marg
-                  [codox "0.10.3"]                             ; lein doc
-                  [com.jakemccrary/lein-test-refresh "0.22.0"] ; lein test-refresh
+                  [codox "0.10.7"]                             ; lein doc
+                  [com.jakemccrary/lein-test-refresh "0.24.1"] ; lein test-refresh
                   [lein-ancient "0.6.15"]
-                  [lein-cloverage "1.0.10"]
+                  [lein-cloverage "1.1.2"]
                   [lein-pprint "1.2.0"]
                   [lein-exec "0.3.7"]
                   [lein-licenses "0.2.2"]
                   [lein-try "0.4.3"]
-                  [jonase/eastwood "0.2.5"]
-                  [lein-cljfmt "0.5.7"]]
+                  [jonase/eastwood "0.3.6"]
+                  [lein-cljfmt "0.6.6"]]
         :dependencies [[slamhound "1.5.5"]
-                       [cljfmt "0.5.7"]
-                       [com.cemerick/pomegranate "1.0.0"]
-                       [org.clojure/java.classpath "0.2.3"]
+                       [cljfmt "0.6.6"]
+                       [com.cemerick/pomegranate "1.1.0"]
+                       [org.clojure/java.classpath "0.3.0"]
                        [junegunn/inspector "0.2.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :injections [(require 'inspector.core)]
